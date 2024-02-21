9c8018220593831387ca7d2445b540e6dcdebc55
Alounsavanh Anothay <boysavan@gmail.com>
2024-02-21 21:58:42 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -5,114 +5,119 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
+  Alert,
+  Button,
+  Image,
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
+import DocumentPicker, { DocumentPickerResponse } from 'react-native-document-picker';
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [singleFile, setSingleFile] = useState<DocumentPickerResponse[]>([]);
+  const [result, setResult] = useState<String>('')
+  const selectFile = async () => {
+    try {
+      // CODE HERE
+      setResult('')
+      setSingleFile([])
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images] // They can more options as well
+      });
+      setSingleFile(res)
+    } catch (err) {
+      console.log('error', err);
+    }
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const uploadImage = async () => {
+    // Check if any file is selected or not
+    if (singleFile != null) {
+      const fileToUpload = singleFile;
+      const data = new FormData();
+      setResult('');
+      // CODE HERE
+      console.log('fileToUpload', JSON.stringify(singleFile));
+      data.append('file', fileToUpload[0]);
+      try {
+        let res = await fetch(
+          'https://remove-background-image2.p.rapidapi.com/remove-background',
+          {
+            method: 'post',
+            body: data,
+            headers: {
+              'X-RapidAPI-Key': '6bacf13d0cmsh24ff92aec1d47a7p1072f0jsn59b4f39582ed',
+              'X-RapidAPI-Host': 'remove-background-image2.p.rapidapi.com',
+              // 'Content-Type': 'multipart/form-data;', อาจไม่ต้องใช้ก็ได้
+            },
+          }
+        );
+        let responseJson = await res.json();
+        console.log('Upload Successful', responseJson);
+
+        if (responseJson.image) {
+          console.log('Upload Successful');
+          setResult(responseJson.image);
+        } else {
+          console.log('Upload Status', 'Image null');
+        }
+      } catch(err) {
+        console.log('error', err);
+      }
+    } else {
+      console.log('Please Select File first');
+    }
   };
 
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView style={{ backgroundColor: Colors.darker }}>
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+        style={{ backgroundColor: Colors.darker }}>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            marginTop: 80,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Button title='Pick' onPress={
+            selectFile
+          } />
+          <View style={{ margin: 18 }}>
+            {
+              singleFile.map((file, index) => (
+                <View key={index}>
+                  <Text
+                    key={index.toString()}
+                    numberOfLines={1}
+                    ellipsizeMode={'middle'}>
+                    {file?.uri}
+                  </Text>
+                  <Button title='Upload' onPress={
+                    uploadImage
+                  } />
+                  <Image
+                    source={{
+                      uri: 'data:image/*;base64,' + result,
+                    }} height={300}
+                  />
+
+                </View>
+              ))
+            }
+
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
